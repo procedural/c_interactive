4732aedfae04d3b9f2a8e82f5204691380fd86a4
Constantine Tarasenkov <constantine@protonmail.com>
2017-11-23 23:57:41 +0300

    Simplify example

--- app.h
+++ app.h
@@ -5,8 +5,8 @@ struct app_state_t {
 
 struct app_api_t {
   struct app_state_t * (*Init)();
-  void (*Finalize)(struct app_state_t *);
   void (*Reload)(struct app_state_t *);
+  int  (*Step)(struct app_state_t *);
   void (*Unload)(struct app_state_t *);
-  int (*Step)(struct app_state_t *);
+  void (*Finalize)(struct app_state_t *);
 };
